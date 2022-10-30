53dd56933950f7a222c90af02781538ba70824f3
Dickson-Pinheiro <dicksonpinheiro4@gmail.com>
2022-10-30 01:07:14 -0300

    menu lateral e suas funcionalidades desenvolvidas

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 let mensagens = [];
 let user = {name: ""}
+let message = {from:"" ,to: "todos", type: "message", text:""}
 const mensagensElement = document.querySelector('.mensagens')
 const botaoEnviarMensagem = document.querySelector("button")
 const inputText = document.querySelector("input[type='text']")
+const menu = document.querySelector(".buttonMenu")
+const blur = document.querySelector(".menuLateral .blur")
+const participantesAtivosBox = document.querySelector(".activePeople")
+let positionPageEnd = false;
 
 function entrarNaSala(){
     let nome = prompt("Qual o seu lindo nome?")
     axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", {name: nome})
     .then(() => {
         user.name = nome
+        message.from = nome
     })
     .catch(()=>{
         entrarNaSala()
@@ -47,25 +53,42 @@ function renderizarMensagens(){
              `
              break;
              case "message":
-                mensagensElement.innerHTML+= `
+                if(mensagem.to === "todos"){
+                    mensagensElement.innerHTML+= `
+                    <div class="mensagem publico">
+                        <p><span>(${mensagem.time})</span><span class="bold">${mensagem.from}</span> para <span class="bold">${mensagem.to}</span>: ${mensagem.text}</p>
+                    </div>
+                    `
+                } else {
+                    mensagensElement.innerHTML+= `
                 <div class="mensagem publico">
                 <p><span>(${mensagem.time})</span><span class="bold">${mensagem.from}</span> para <span class="bold">${mensagem.to}</span>: ${mensagem.text}</p>
             </div>
                 `
+                }
+                
             break;
             case "private_message":
-                if(mensagem.from === user.name)
+                if(mensagem.from === user.name || mensagem.to === user.name){
                 mensagensElement.innerHTML+= `
                 <div class="mensagem reservado">
                 <p><span>(${mensagem.time})</span><span class="bold">${mensagem.from}</span> reservadamente para <span class="bold">${mensagem.to}</span>: ${mensagem.text}</p>
             </div>
                 `
+                }
+                break;
+            default:
+                break;
         }
     })
 }
 
 function posicionarPagina(element){
-    element.scrollIntoView()
+    if(!positionPageEnd){
+        positionPageEnd = true;
+        element.scrollIntoView()
+    }
+    
 }
 
 function manterConectado(){
@@ -79,13 +102,7 @@ inputText.addEventListener("keypress", enviarMensagemEnter)
 
 function enviarMensagem(){
     let inputText = document.querySelector("input[type='text']")
-    let mensagem = {
-        from: user.name,
-        to: "todos",
-        text: inputText.value,
-        type: "message"
-    }
-    axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", mensagem)
+    axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", {...message, text: inputText.value})
     .then(() => inputText.value="")
     .catch(() => {
         window.location.reload()
@@ -96,4 +113,56 @@ function enviarMensagemEnter(e){
     if(e.key === "Enter"){
         enviarMensagem()
     }
+}
+
+
+function buscarParticipantesAtivos(){
+    axios.get("https://mock-api.driven.com.br/api/v6/uol/participants")
+    .then((response) => {
+        let participants = response.data
+        participantesAtivosBox.innerHTML = ""
+        participantesAtivosBox.innerHTML = '<div class="people" onclick="addSelectionParticipants(this)"><img src="./images/icon-header.svg" /><p>Todos</p><ion-icon name="checkmark-outline" value="todos"></ion-icon></div>'
+        participants.forEach(user => {
+            participantesAtivosBox.innerHTML+= `
+            <div class="people" onclick='addSelectionParticipants(this)'>
+                <img src="./images/people.svg" />
+                <p>${user.name}</p>
+                <ion-icon name="checkmark-outline"></ion-icon>
+            </div>
+            `
+        })
+    })
+}
+
+buscarParticipantesAtivos()
+setInterval(buscarParticipantesAtivos, 10000);
+
+function addSelectionParticipants(element){
+    let anterior = document.querySelector(".activePeople .selected")
+    if(anterior){
+        anterior.classList.remove("selected")
+    }
+        element.children[2].classList.add("selected")
+        message.to = element.children[1].innerHTML
+}
+
+function addSelectionVisibilidade(element){
+    let anterior = document.querySelector(".visibilidade .selected")
+    if(anterior){
+        anterior.classList.remove("selected")
+    }
+        element.children[2].classList.add("selected")
+        if(element.children[1].innerHTML === "Público"){
+            message.type = "message"
+        } else {
+            message.type= "private_message"
+        }
+}
+
+menu.addEventListener("click", controleMenu)
+blur.addEventListener("click", controleMenu)
+
+function controleMenu(){
+    let menuLateral = document.querySelector(".menuLateral")
+    menuLateral.classList.toggle("showMenuLateral")
 }
